8288acbe07bf464611f1ee7029ecd13d7dbbc7e1
Phet <rawkidz@hotmail.com>
2024-07-13 02:01:47 +0700

    [update] - add children staste

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
 import componentImg from './assets/components.png'
-import Header from './components/Header.jsx'
+import Header from './components/Header/Header.jsx'
 import CoreConcept from './components/CoreConcept.jsx'
+import TabButton from './components/TabButton.jsx'
 const data = [
   {
     title: 'test',
@@ -54,7 +55,15 @@ function App() {
             <CoreConcept {...data[0]} />
           </ul>
         </section>
-
+        <section id="examples">
+          <h2>Example</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
